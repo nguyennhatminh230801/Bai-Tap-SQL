d5f218d02159676004591d5cea7846fcc44fe036
= <=>
2021-01-25 15:03:42 +0700

    Bai Tap Tuan 2

---
+++ Bai Tap Tuan 3 SQL/Bai 3.1.sql
@@ -0,0 +1,109 @@
+﻿use master
+go
+--A. Tạo CSDL có tên DeptEmp
+create database DeptEmp
+on primary(
+	name = DeptEmp_DB,
+	filename = 'D:\BaiTapHeQuanTriCSDL\Bai 3\DeptEmp_DB.mdf',
+	size = 10mb,
+	maxsize = 100mb,
+	filegrowth = 10mb
+)
+
+log on(
+	name = DeptEmp_log,
+	filename = 'D:\BaiTapHeQuanTriCSDL\Bai 3\DeptEmp_log.ldf',
+	size = 10mb,
+	maxsize = 100mb,
+	filegrowth = 10mb
+)
+
+
+go
+
+use DeptEmp
+go
+--B. Tạo các bảng dữ liệu sau trong CSDL vừa tạo với các chỉ định ràng buộc tương ứng
+create table Department(
+	DepartmentNo Integer primary key,
+	DepartmentName char(25) not null,
+	Location char(25) not null
+)
+
+go
+
+create table Employee(
+	EmpNo Integer primary key,
+	Fname varchar(15) not null,
+	Lname varchar(15) not null,
+	Job varchar(25) not null,
+	Hiredate datetime not null,
+	Salary numeric not null,
+	Commision numeric,
+	DepartmentNo Integer,
+
+	constraint FK_DepartmentNo foreign key(DepartmentNo) references Department(DepartmentNo)
+)
+
+
+--C. Chèn dữ liệu sau đây vào các bảng trên
+insert into Department values(10, 'Accounting', 'Melbourne')
+insert into Department values(20, 'Research', 'Adealide')
+insert into Department values(30, 'Sales' , 'Sydney')
+insert into Department values(40, 'Operations', 'Perth')
+
+insert into Employee values(1, 'John' , 'Smith' , 'Clerk' , '1980-12-17', 800 , null , 20)
+insert into Employee values(2, 'Peter', 'Allen', 'Salesman', '1981-11-20' , 1600, 300 , 30)
+insert into Employee values(3, 'Kate', 'Ward' ,'Salesman', '1981-11-22', 1250 ,500 , 30)
+insert into Employee values(4, 'Jack' , 'Jones', 'Manager', '1981-7-2' , 2975 , null, 20)
+insert into Employee values(5, 'Joe' , 'Martin' , 'Salesman', '1981-09-28', 1250 , 1400, 30)
+
+--D. Thực hiện các truy vấn sau trên cơ sở dữ liệu trên:
+
+--1. Hiển thị nội dung bảng Department
+select * from Department
+
+--2. Hiển thị nội dung bảng Employee
+select * from Employee
+
+--3. Hiển thị employee number, employee first name và employee last name từ bảng Employee mà employee first name có tên là ‘Kate’.
+select EmpNo, Fname, Lname 
+from Employee
+where Fname like 'Kate'
+
+--4. Hiển thị ghép 2 trường Fname và Lname thành Full Name, Salary, 10%Salary (tăng 10% so với lương ban đầu).
+select Fname + ' ' + Lname as 'Full Name' , Salary, Salary + (10 * Salary / 100) as 'Tăng 10% so với lương ban đầu' 
+from Employee
+
+--5. Hiển thị Fname, Lname, HireDate cho tất cả các Employee có HireDate là năm 1981 và sắp xếp theo thứ tự tăng dần của Lname.
+select Fname, Lname, Hiredate 
+from Employee
+where YEAR(Hiredate) = 1981
+order by Lname ASC
+
+--6. Hiển thị trung bình(average), lớn nhất (max) và nhỏ nhất(min) của lương(salary) cho từng phòng ban trong bảng Employee.
+select AVG(Salary) as 'Trung Bình Lương', MAX(Salary) as 'Lương Lớn Nhất', Min(Salary) as 'Lương Nhỏ Nhất' 
+from Employee
+group by DepartmentNo
+
+--7. Hiển thị DepartmentNo và số người có trong từng phòng ban có trong bảng Employee.
+select DepartmentNo, COUNT(DepartmentNo) as 'Số người' 
+from Employee
+group by DepartmentNo
+
+--8. Hiển thị DepartmentNo, DepartmentName, FullName (Fname và Lname), Job, Salary trong bảng Department và bảng Employee.
+select Department.DepartmentNo, DepartmentName, Fname + ' ' + Lname as 'Full Name', Job, Salary 
+from Department inner join Employee
+on Department.DepartmentNo = Employee.DepartmentNo
+
+--9. Hiển thị DepartmentNo, DepartmentName, Location và số người có trong từng phòng ban của bảng Department và bảng Employee.
+select Department.DepartmentNo,	DepartmentName, Location , COUNT(*) as 'Số Lượng'
+from Employee inner join Department
+on Employee.DepartmentNo = Department.DepartmentNo
+group by Department.DepartmentNo, DepartmentName, Location 
+
+--10. Hiển thị tất cả DepartmentNo, DepartmentName, Location và số người có trong từng phòng ban của bảng Department và bảng Employee
+select Department.DepartmentNo,	DepartmentName, Location , COUNT(*) as 'Số Lượng'
+from Employee inner join Department
+on Employee.DepartmentNo = Department.DepartmentNo
+group by Department.DepartmentNo, DepartmentName, Location
---
+++ Bai Tap Tuan 3 SQL/Bai 3.2.sql
@@ -0,0 +1,86 @@
+﻿use master
+go
+
+--A. Tạo CSDL có tên MarkManagement
+create database MarkManagement
+on primary(
+	name = 'MarkManagement_DB',
+	filename = 'D:\BaiTapHeQuanTriCSDL\Bai 3\MarkManagement_DB.mdf',
+	size = 10mb,
+	maxsize = 100mb,
+	filegrowth = 10mb
+)
+
+log on(
+	name = 'MarkManagement_log',
+	filename = 'D:\BaiTapHeQuanTriCSDL\Bai 3\MarkManagement_log.ldf',
+	size = 10mb,
+	maxsize = 100mb,
+	filegrowth = 10mb
+)
+
+use MarkManagement
+
+--B. Tạo các bảng dữ liệu sau trong CSDL vừa tạo với các chỉ định ràng buộc tương ứng
+create table Students(
+	StudentID nvarchar(12) primary key,
+	StudentName nvarchar(25) not null,
+	DateofBirth datetime not null,
+	Email nvarchar(40),
+	Phone nvarchar(12),
+	Class nvarchar(10)
+)
+
+create table Subjects(
+	SubjectID nvarchar(10) primary key,
+	SubjectName nvarchar(25) not null
+)
+
+create table Mark(
+	StudentID nvarchar(12),
+	SubjectID nvarchar(10),
+	Date datetime,
+	Theory tinyint,
+	Practical tinyint
+
+	constraint PK_StudentID_SubjectID primary key(StudentID, SubjectID),
+	constraint FK_StudentID foreign key(StudentID) references Students(StudentID) on delete cascade on update cascade,
+	constraint FK_SubjectID foreign key(SubjectID) references Subjects(SubjectID) on delete cascade on update cascade
+)
+
+drop table Mark
+--C. Chèn dữ liệu sau đây vào các bảng trên 
+
+insert into Students values(N'AV0807005', N'Mail Trung Hiếu', '1989-10-11', N'trunghieu@yahoo.com', N'0904115116', N'AV1')
+insert into Students values(N'AV0807006', N'Nguyễn Quý Hùng', '1988-12-2', N'quyhung@yahoo.com', N'0955667787' , N'AV2')
+insert into Students values(N'AV0807007', N'Đỗ Đắc Huỳnh', '1990-1-2', N'dachuynh@yahoo.com', N'0988574747' , N'AV2')
+insert into Students values(N'AV0807009', N'An Đăng Khuê', '1986-3-6', N'dangkhue@yahoo.com', N'0986757463', N'AV1')
+insert into Students values(N'AV0807010', N'Nguyễn T. Tuyết Lan' , '1989-7-12', N'tuyetlan@gmail.com',  N'0983310342', N'AV2')
+insert into Students values(N'AV0807011', N'Đinh Phụng Long' , '1990-12-2', N'phunglong@yahoo.com', N'', N'AV1')
+insert into Students values(N'AV0807012', N'Nguyễn Tuấn Nam', '1990-3-2', N'tuannam@yahoo.com', N'',  N'AV1')
+
+insert into Subjects values(N'S001', N'SQL')
+insert into Subjects values(N'S002' , N'Java Simplefield')
+insert into Subjects values(N'S003' , N'Active Server Page')
+
+insert into Mark values(N'AV0807005' , N'S001', '2008-5-6', 8, 25)
+insert into Mark values(N'AV0807006', N'S002','2008-5-6', 16, 30)
+insert into Mark values(N'AV0807007', N'S001','2008-5-6', 10 ,25)
+insert into Mark values(N'AV0807009', N'S003', '2008-5-6', 7, 13)
+insert into Mark values(N'AV0807010', N'S003', '2008-5-6', 9, 16)
+insert into Mark values(N'AV0807011', N'S002', '2008-5-6', 8 , 30)
+insert into Mark values(N'AV0807012' , N'S001', '2008-5-6', 7, 31)
+insert into Mark values(N'AV0807005', N'S002', '2008-6-6', 12, 11)
+insert into Mark values(N'AV0807009', N'S003', '2008-6-6', 11 , 20)
+insert into Mark values(N'AV0807010', N'S001', '2008-6-6', 7, 6)
+
+--D. Thực hiện các truy vấn sau trên cơ sở dữ liệu trên
+
+--1. Hiển thị nội dung bảng Students
+select * from Students
+
+--2. Hiển thị nội dung danh sách sinh viên lớp AV1
+select * from Students
+where Class like N'AV1'
+
+--3. Sử dụng lệnh UPDATE để chuyển sinh viên có mã AV0807012 sang lớp AV2update Students set Class = N'AV2' where StudentID like N'AV0807012'--4. Tính tổng số sinh viên của từng lớpselect Class as 'Lớp', COUNT(*) as 'Số Lượng' from Studentsgroup by Class--5. Hiển thị danh sách sinh viên lớp AV2 được sắp xếp tăng dần theo StudentNameselect * from Studentswhere Class like N'AV2'order by StudentName ASC--6. Hiển thị danh sách sinh viên không đạt lý thuyết môn S001 (theory <10) thi ngày 6/5/2008select * from Markwhere Theory < 10 and Date = '2008-5-6' and SubjectID like N'S001'--7. Hiển thị tổng số sinh viên không đạt lý thuyết môn S001. (theory <10)select SubjectID as 'Môn Thi', COUNT(*) as 'Số lượng sinh viên không đạt' from Markwhere SubjectID like N'S001' and Theory < 10group by SubjectID--8. Hiển thị Danh sách sinh viên học lớp AV1 và sinh sau ngày 1/1/1980select * from Studentswhere Class like N'AV1' and DateofBirth > '1980-1-1'--9. Xoá sinh viên có mã AV0807011delete from Studentswhere StudentID like N'AV0807011'--10.Hiển thị danh sách sinh viên dự thi môn có mã S001 ngày 6/5/2008 bao gồm các trường sau: StudentID, StudentName, SubjectName, Theory, Practical, Dateselect Mark.StudentID, StudentName, SubjectName, Theory, Practical, Date fromMark inner join Students on Mark.StudentID = Students.StudentIDinner join Subjects on Mark.SubjectID = Subjects.SubjectIDwhere Date = '2008-5-6'
