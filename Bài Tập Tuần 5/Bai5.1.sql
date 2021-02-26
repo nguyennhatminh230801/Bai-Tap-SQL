59309f12d3e5b092ce60f51e581b3e7a57d5a2a6
= <=>
2021-02-26 17:14:38 +0700

    new bai tap tuan 5

---
+++ Bài Tập Tuần 5/Bai5.1.sql
@@ -0,0 +1,97 @@
+﻿use master
+
+create database QLKHO
+on primary(
+	name = 'QLKHO_DB',
+	filename = 'C:\Users\Admin\Desktop\BTVN Tuan 5\QLKHO_DB.mdf',
+	size = 10mb,
+	maxsize = 100mb,
+	filegrowth = 10mb
+)
+
+log on(
+	name = 'QLKHO_log',
+	filename = 'C:\Users\Admin\Desktop\BTVN Tuan 5\QLKHO_log.ldf',
+	size = 10mb,
+	maxsize = 100mb,
+	filegrowth = 10mb
+)
+
+use QLKHO
+create table Ton(
+	MaVT nvarchar(10) primary key not null,
+	TenVT nvarchar(20) not null,
+	SoLuongT int
+)
+
+create table Nhap(
+	SoHDN nvarchar(10) not null,
+	MaVT nvarchar(10) not null,
+	SoluongN int,
+	DongiaN money,
+	NgayN date
+
+	constraint PK_SoHDN_MaVT primary key(SoHDN, MaVT),
+	constraint FK_MaVT_Nhap foreign key(MaVT) references Ton(MaVT)
+)
+
+create table Xuat(
+	SoHDX nvarchar(10) not null,
+	MaVT nvarchar(10) not null,
+	SoluongX int,
+	DongiaX money ,
+	NgayX date
+
+	constraint PK_SoHDX_MaVT primary key(SoHDX, MaVT),
+	constraint FK_MaVT_Xuat foreign key(MaVT) references Ton(MaVT)
+)
+
+insert into Ton values(N'VT01', N'Xi Măng', 100)
+insert into Ton values(N'VT02', N'Cốt thép', 200)
+insert into Ton values(N'VT03', N'Máy Dập', 120)
+insert into Ton values(N'VT04', N'Gương', 50)
+insert into Ton values(N'VT05', N'Gạch', 390)
+
+insert into Nhap values(N'N01', 'VT01', 200, 50000, '2021-2-24')
+insert into Nhap values(N'N02', 'VT04', 1000, 30000, '2021-2-25')
+insert into Nhap values(N'N03', 'VT03', 280, 40000, '2021-8-23')
+
+insert into Xuat values(N'X01', 'VT04', 500, 40000, '2021-3-25')
+insert into Xuat values(N'X02', 'VT05', 190, 90000, '2021-8-23')
+
+--Câu 2: Thống kê tiền bán theo mã vật tư gồm MaVT, TenVT, TienBan (TienBan=SoLuongX*DonGiaX)
+create view CAU2
+as
+	select Ton.MaVT,TenVT, SUM(SoluongX * DongiaX) as N'Tiền Bán'
+	from Xuat inner join Ton on Xuat.MaVT = Ton.MaVT
+	group by Ton.MaVT,TenVT
+
+select * from CAU2
+
+--Câu 3- Thống kê soluongxuat theo tên vattu
+create view CAU3
+as
+	select Ton.MaVT, Ton.TenVT, SUM(SoluongX) as N'Tổng Số Lượng Xuất'
+	from Ton inner join Xuat on Ton.MaVT = Xuat.MaVT
+	group by Ton.MaVT, Ton.TenVT
+
+select * from CAU3
+
+--Câu 4 - Thống kê soluongnhap theo tên vật tư
+create view CAU4
+as
+	select Ton.MaVT, Ton.TenVT, SUM(SoluongN) as N'Tổng Số Lượng Nhập'
+	from Ton inner join Nhap on Ton.MaVT = Nhap.MaVT
+	group by Ton.MaVT, Ton.TenVT
+
+select * from CAU4
+
+--Câu 5- Đưa ra tổng soluong còn trong kho biết còn = nhap – xuất + tồn theo từng nhóm vật tư
+create view CAU5
+as
+	select Ton.MaVT, Ton.TenVT, SUM(SoluongN) - SUM(SoluongX) + SUM(SoLuongT) as 'Tổng Số Luọng'
+	from Ton inner join Xuat on Ton.MaVT = Xuat.MaVT
+			 inner join Nhap on Ton.MaVT = Nhap.MaVT
+	group by Ton.MaVT, Ton.TenVT
+
+select * from CAU5
---
+++ Bài Tập Tuần 5/Bai5.2.sql
@@ -0,0 +1,81 @@
+﻿use master
+go
+
+create database QLSV
+on primary(
+	name = 'QLSV_DB',
+	filename = 'C:\Users\Admin\Desktop\BTVN Tuan 5\QLSV_DB.mdf',
+	size = 10mb,
+	maxsize = 100mb,
+	filegrowth = 10mb
+)
+
+log on(
+	name = 'QLSV_log',
+	filename = 'C:\Users\Admin\Desktop\BTVN Tuan 5\QLSV_log.ldf',
+	size = 10mb,
+	maxsize = 100mb,
+	filegrowth = 10mb
+)
+
+use QLSV
+create table Lop(
+	MaLop nchar(10) primary key not null,
+	TenLop nvarchar(20) not null,
+	Phong nchar(10)
+)
+
+create table SV(
+	MaSV nchar(10) primary key not null,
+	TenSV nvarchar(30) not null,
+	MaLop nchar(10) not null
+
+	constraint FK_MaLop foreign key(MaLop) references Lop(MaLop)
+)
+
+
+insert into Lop values(N'1', N'CĐ', N'1')
+insert into Lop values(N'2', N'ĐH', N'2')
+insert into Lop values(N'3', N'LT', N'2')
+insert into Lop values(N'4', N'CH', N'4')
+
+insert into SV values(N'1', N'A', N'1')
+insert into SV values(N'2', N'B', N'2')
+insert into SV values(N'3', N'C', N'1')
+insert into SV values(N'4', N'D', N'3')
+
+--1. Viết hàm thống kê xem mỗi lớp có bao nhiêu sinh viên với malop là tham số truyền vào từ bàn phím.create function ThongKe(@MaLop nchar(10))
+returns int
+as
+begin
+		declare @Soluong int
+		set @Soluong = (select COUNT(SV.MaSV) 
+						from SV inner join Lop 
+						on SV.MaLop = Lop.MaLop 
+						where Lop.MaLop = @MaLop 
+						group by Lop.TenLop)
+		return @Soluong
+end
+
+select dbo.ThongKe(N'1') as 'Số Lượng Sinh Viên'
+
+--2. Đưa ra danh sách sinh viên(masv,tensv) học lớp với tenlop được truyền vào từ bàn phím.
+create function DSSV(@Tenlop nvarchar(30))
+returns @BangDSSV table(MaSV nchar(10), TenSV nvarchar(30))
+as
+	begin 
+		insert into @BangDSSV 
+		select MaSV, TenSV 
+		from SV inner join Lop 
+		on SV.MaLop = Lop.MaLop 
+		where Lop.TenLop = @Tenlop
+		return
+	end
+
+select * from dbo.DSSV(N'ĐH')
+
+--3. Đưa ra hàm thống kê sinhvien: malop,tenlop,soluong sinh viên trong lớp, với tên lớp
+--được nhập từ bàn phím. Nếu lớp đó chưa tồn tại thì thống kê tất cả các lớp, ngược lại nếu
+--lớp đó đã tồn tại thì chỉ thống kê mỗi lớp đó.create function Cau3(@Tenlop nvarchar(30))returns @ThongKeSV table(MaLop nchar(10), TenLop nvarchar(20), Soluong int)as	begin		if(not exists(select MaLop from Lop where TenLop = @Tenlop))			insert into @ThongKeSV 			select Lop.MaLop, Lop.TenLop, count(SV.MaSV) 			from Lop inner join SV on Lop.MaLop = SV.MaLop 			group by Lop.MaLop, Lop.TenLop		else			insert into @ThongKeSV 			select Lop.MaLop, TenLop, count(SV.MaSV) 			from Lop inner join SV 			on Lop.MaLop = SV.MaLop 			where Lop.TenLop = @TenLop 			group by Lop.MaLop, Lop.TenLop			return	endselect * from dbo.Cau3(N'ĐH')--4. Đưa ra phòng học của tên sinh viên nhập từ bàn phím.create function Cau4(@Tensv nvarchar(30))returns nvarchar(30)as	begin		declare @PhongHoc nvarchar(30)		set @PhongHoc = (select Phong 						 from Lop inner join SV						 on Lop.MaLop = SV.MaLop						 where TenSV = @Tensv)		return @PhongHoc	endselect dbo.Cau4(N'A') as N'Phòng Học'--5. Đưa ra thống kê masv,tensv, tenlop với tham biến nhập từ bàn phím là phòng. Nếu phòng
+--không tồn tại thì đưa ra tất cả các sinh viên và các phòng. Neu phòng tồn tại thì đưa ra các
+--sinh vien của các lớp học phòng đó (Nhiều lớp học cùng phòng).create function Cau5(@phong nchar(10))returns @ThongKeSinhVien table(MaSV nchar(10), TenSV nvarchar(30), TenLop nvarchar(30))as	begin		if(not exists(select Phong from Lop where Phong = @phong))			insert into @ThongKeSinhVien			select SV.MaSV, SV.TenSV, Lop.TenLop			from SV inner join Lop			on SV.MaLop = Lop.MaLop		else			insert into @ThongKeSinhVien			select SV.MaSV, SV.TenSV, Lop.TenLop			from SV inner join Lop			on SV.MaLop = Lop.MaLop			where Phong = @phong			return	endselect * from dbo.Cau5(N'3')--6. Viết hàm thống kê xem mỗi phòng có bao nhiêu lớp học. Nếu phòng không tồn tại trả về giá trị 0.create function Cau6(@phong nvarchar(30))returns intas	begin		declare @Soluong int		if(not exists(select Phong from Lop where Phong = @phong))			set @Soluong = 0		else			set @Soluong = (select count(*) 							from SV inner join Lop							on SV.MaLop = Lop.MaLop							where Phong = @phong							group by Lop.MaLop)			return @Soluong	end--drop function Cau6select dbo.Cau6(N'1') as N'Số lớp học'
