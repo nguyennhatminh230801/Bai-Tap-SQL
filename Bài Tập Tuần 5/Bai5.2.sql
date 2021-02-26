5b1979088e65e1b4a236fd941bea94b880ac8d1e
= <=>
2021-02-26 17:21:55 +0700

    BTVN Tuan 5

--- Bài Tập Tuần 5/Bai5.2.sql
+++ Bài Tập Tuần 5/Bai5.2.sql
@@ -44,7 +44,8 @@ insert into SV values(N'2', N'B', N'2')
 insert into SV values(N'3', N'C', N'1')
 insert into SV values(N'4', N'D', N'3')
 
---1. Viết hàm thống kê xem mỗi lớp có bao nhiêu sinh viên với malop là tham số truyền vào từ bàn phím.create function ThongKe(@MaLop nchar(10))
+--1. Viết hàm thống kê xem mỗi lớp có bao nhiêu sinh viên với malop là tham số truyền vào từ bàn phím.
+create function ThongKe(@MaLop nchar(10))
 returns int
 as
 begin
@@ -76,6 +77,84 @@ select * from dbo.DSSV(N'ĐH')
 
 --3. Đưa ra hàm thống kê sinhvien: malop,tenlop,soluong sinh viên trong lớp, với tên lớp
 --được nhập từ bàn phím. Nếu lớp đó chưa tồn tại thì thống kê tất cả các lớp, ngược lại nếu
---lớp đó đã tồn tại thì chỉ thống kê mỗi lớp đó.create function Cau3(@Tenlop nvarchar(30))returns @ThongKeSV table(MaLop nchar(10), TenLop nvarchar(20), Soluong int)as	begin		if(not exists(select MaLop from Lop where TenLop = @Tenlop))			insert into @ThongKeSV 			select Lop.MaLop, Lop.TenLop, count(SV.MaSV) 			from Lop inner join SV on Lop.MaLop = SV.MaLop 			group by Lop.MaLop, Lop.TenLop		else			insert into @ThongKeSV 			select Lop.MaLop, TenLop, count(SV.MaSV) 			from Lop inner join SV 			on Lop.MaLop = SV.MaLop 			where Lop.TenLop = @TenLop 			group by Lop.MaLop, Lop.TenLop			return	endselect * from dbo.Cau3(N'ĐH')--4. Đưa ra phòng học của tên sinh viên nhập từ bàn phím.create function Cau4(@Tensv nvarchar(30))returns nvarchar(30)as	begin		declare @PhongHoc nvarchar(30)		set @PhongHoc = (select Phong 						 from Lop inner join SV						 on Lop.MaLop = SV.MaLop						 where TenSV = @Tensv)		return @PhongHoc	endselect dbo.Cau4(N'A') as N'Phòng Học'--5. Đưa ra thống kê masv,tensv, tenlop với tham biến nhập từ bàn phím là phòng. Nếu phòng
+--lớp đó đã tồn tại thì chỉ thống kê mỗi lớp đó.
+
+create function Cau3(@Tenlop nvarchar(30))
+returns @ThongKeSV table(MaLop nchar(10), TenLop nvarchar(20), Soluong int)
+as
+	begin
+		if(not exists(select MaLop from Lop where TenLop = @Tenlop))
+			insert into @ThongKeSV 
+			select Lop.MaLop, Lop.TenLop, count(SV.MaSV) 
+			from Lop inner join SV on Lop.MaLop = SV.MaLop 
+			group by Lop.MaLop, Lop.TenLop
+		else
+			insert into @ThongKeSV 
+			select Lop.MaLop, TenLop, count(SV.MaSV) 
+			from Lop inner join SV 
+			on Lop.MaLop = SV.MaLop 
+			where Lop.TenLop = @TenLop 
+			group by Lop.MaLop, Lop.TenLop
+			return
+	end
+
+select * from dbo.Cau3(N'ĐH')
+
+--4. Đưa ra phòng học của tên sinh viên nhập từ bàn phím.
+create function Cau4(@Tensv nvarchar(30))
+returns nvarchar(30)
+as
+	begin
+		declare @PhongHoc nvarchar(30)
+		set @PhongHoc = (select Phong 
+						 from Lop inner join SV
+						 on Lop.MaLop = SV.MaLop
+						 where TenSV = @Tensv)
+		return @PhongHoc
+	end
+
+select dbo.Cau4(N'A') as N'Phòng Học'
+
+--5. Đưa ra thống kê masv,tensv, tenlop với tham biến nhập từ bàn phím là phòng. Nếu phòng
 --không tồn tại thì đưa ra tất cả các sinh viên và các phòng. Neu phòng tồn tại thì đưa ra các
---sinh vien của các lớp học phòng đó (Nhiều lớp học cùng phòng).create function Cau5(@phong nchar(10))returns @ThongKeSinhVien table(MaSV nchar(10), TenSV nvarchar(30), TenLop nvarchar(30))as	begin		if(not exists(select Phong from Lop where Phong = @phong))			insert into @ThongKeSinhVien			select SV.MaSV, SV.TenSV, Lop.TenLop			from SV inner join Lop			on SV.MaLop = Lop.MaLop		else			insert into @ThongKeSinhVien			select SV.MaSV, SV.TenSV, Lop.TenLop			from SV inner join Lop			on SV.MaLop = Lop.MaLop			where Phong = @phong			return	endselect * from dbo.Cau5(N'3')--6. Viết hàm thống kê xem mỗi phòng có bao nhiêu lớp học. Nếu phòng không tồn tại trả về giá trị 0.create function Cau6(@phong nvarchar(30))returns intas	begin		declare @Soluong int		if(not exists(select Phong from Lop where Phong = @phong))			set @Soluong = 0		else			set @Soluong = (select count(*) 							from SV inner join Lop							on SV.MaLop = Lop.MaLop							where Phong = @phong							group by Lop.MaLop)			return @Soluong	end--drop function Cau6select dbo.Cau6(N'1') as N'Số lớp học'
+--sinh vien của các lớp học phòng đó (Nhiều lớp học cùng phòng).
+
+create function Cau5(@phong nchar(10))
+returns @ThongKeSinhVien table(MaSV nchar(10), TenSV nvarchar(30), TenLop nvarchar(30))
+as
+	begin
+		if(not exists(select Phong from Lop where Phong = @phong))
+			insert into @ThongKeSinhVien
+			select SV.MaSV, SV.TenSV, Lop.TenLop
+			from SV inner join Lop
+			on SV.MaLop = Lop.MaLop
+		else
+			insert into @ThongKeSinhVien
+			select SV.MaSV, SV.TenSV, Lop.TenLop
+			from SV inner join Lop
+			on SV.MaLop = Lop.MaLop
+			where Phong = @phong
+			return
+	end
+
+select * from dbo.Cau5(N'3')
+
+--6. Viết hàm thống kê xem mỗi phòng có bao nhiêu lớp học. Nếu phòng không tồn tại trả về giá trị 0.
+create function Cau6(@phong nvarchar(30))
+returns int
+as
+	begin
+		declare @Soluong int
+		if(not exists(select Phong from Lop where Phong = @phong))
+			set @Soluong = 0
+		else
+			set @Soluong = (select count(*) 
+							from SV inner join Lop
+							on SV.MaLop = Lop.MaLop
+							where Phong = @phong
+							group by Lop.MaLop)
+			return @Soluong
+	end
+
+--drop function Cau6
+select dbo.Cau6(N'1') as N'Số lớp học'
