b01ccb8d4d94b8f4275d8c04269c6c1a4851754c
= <=>
2021-04-12 21:40:21 +0700

    bai 10 trigger

---
+++ 10.Bài Tập Tuần 10/Bai 10.1.sql
@@ -0,0 +1,156 @@
+﻿use master
+
+create database QLBanHang
+on primary(
+	name = 'QLBanHang_DB',
+	filename = 'C:\Users\Admin\Desktop\Bai Tap Tuan 10\QLBanHang_DB.mdf',
+	size = 10mb,
+	maxsize = 100mb,
+	filegrowth = 10mb
+)
+log on(
+	name = 'QLBanHang_Log',
+	filename = 'C:\Users\Admin\Desktop\Bai Tap Tuan 10\QLBanHang_LOG.ldf',
+	size = 10mb,
+	maxsize = 100mb,
+	filegrowth = 10mb
+)
+
+use QLBanHang
+
+create table Hang(
+	MaHang nchar(10) primary key,
+	TenHang nvarchar(30),
+	SoLuong int,
+	GiaBan money
+)
+
+create table HoaDon(
+	MaHD nchar(10) primary key,
+	MaHang nchar(10),
+	SoLuongBan int,
+	NgayBan date
+
+	constraint FK_MaHang foreign key(MaHang) references Hang(MaHang)
+)
+
+insert into Hang values(N'H01', N'Ti Vi', 200, 5000000)
+insert into Hang values(N'H02', N'Tủ Lạnh', 200, 5000000)
+insert into Hang values(N'H03', N'Điều Hòa', 200, 5000000)
+insert into Hang values(N'H04', N'Bình Nóng Lạnh', 200, 5000000)
+insert into Hang values(N'H05', N'Máy Giặt', 200, 5000000)
+
+--delete from HoaDon
+--delete from Hang
+
+--Bài 1. Hãy tạo 1 trigger insert HoaDon, hãy kiểm tra xem mahang cần mua có tồn
+--tại trong bảng HANG không, nếu không hãy đưa ra thông báo.
+-- Nếu thỏa mãn hãy kiểm tra xem: soluongban <= soluong? Nếu không hãy
+--đưa ra thông báo.
+-- Ngược lại cập nhật lại bảng HANG với: soluong = soluong - soluongban
+
+create trigger Bai1 --nên làm lại bằng cách inner join để với trường hợp nhiều hóa đơn
+on HoaDon
+for insert
+as
+	begin
+		declare @MaHang nchar(10)
+		set @MaHang = (select MaHang from inserted)
+
+		if(not exists(select * from Hang where MaHang = @MaHang))
+			begin
+				raiserror(N'Không tìm thấy mã hàng trong CSDL',16,1)
+				rollback transaction
+			end
+		else
+			begin
+				declare @SoLuongBan int
+				declare @SoLuong int
+
+				set @SoLuongBan = (select SoLuongBan from inserted)
+				set @SoLuong = (select SoLuong from Hang where MaHang = @MaHang)
+
+				if(@SoLuongBan > @SoLuong)
+					begin
+						raiserror(N'Không đủ số lượng để xuất hàng',16,1)
+						rollback transaction 
+					end
+				else
+					begin
+						update Hang
+						set SoLuong = @SoLuong - @SoLuongBan
+						where MaHang = @MaHang
+					end
+			end
+	end
+
+--test bài 1
+select * from Hang
+select * from HoaDon
+
+--TH không đủ số lượng
+insert into HoaDon values(N'HD06', N'H05', 300, '2021-4-5')
+
+--TH thông thường
+insert into HoaDon values(N'HD06', N'H05', 10, '2021-4-5')
+
+select * from Hang
+select * from HoaDon
+
+--Bài 2. Viết trigger kiểm soát việc Delete bảng HOADON, Hãy cập nhật lại soluong
+--trong bảng HANG với: SOLUONG =SOLUONG + DELETED.SOLUONGBAN
+
+create trigger Bai2 --nên làm lại bằng cách inner join để với trường hợp nhiều hóa đơn
+on HoaDon
+for delete
+as
+	begin
+		update Hang
+		set SoLuong = SoLuong + deleted.SoLuongBan 
+		from Hang inner join deleted
+		on Hang.MaHang = deleted.MaHang
+		where Hang.MaHang = deleted.MaHang
+	end
+
+--TH test bài 2
+select * from Hang
+select * from HoaDon
+
+delete from HoaDon where MaHD = N'HD06'
+
+select * from HoaDon
+select * from Hang
+
+--Bài 3. Hãy viết trigger kiểm soát việc Update bảng HOADON. Khi đó hãy update
+--lại soluong trong bảng HANG.
+
+--note:
+--+) deleted: data trước update
+--+) inserted: data sau update
+
+create trigger Bai3
+on HoaDon
+for update
+as
+	begin
+		declare @SoLuongTruoc int
+		declare @SoLuongSau int
+
+		set @SoLuongTruoc = (select SoLuongBan from deleted)
+		set @SoLuongSau = (select SoLuongBan from inserted)
+
+		update Hang
+		set SoLuong = SoLuong - (@SoLuongSau - @SoLuongTruoc)
+		from Hang inner join inserted
+		on Hang.MaHang = inserted.MaHang
+	end
+
+--test bai 3
+select * from Hang
+select * from HoaDon
+
+update HoaDon set SoLuongBan = SoLuongBan - 5 where MaHD = 'HD06'
+
+select * from HoaDon
+select * from Hang
+
---
+++ 10.Bài Tập Tuần 10/Bai 10.2.sql
@@ -0,0 +1,260 @@
+﻿use master
+
+create database QLBH
+on primary(
+	name = 'QLBH_DB',
+	filename = 'C:\Users\Admin\Desktop\Bai Tap Tuan 10\QLBH_DB.mdf',
+	size = 10mb,
+	maxsize = 100mb,
+	filegrowth = 10mb
+)
+log on(
+	name = 'QLBH_Log',
+	filename = 'C:\Users\Admin\Desktop\Bai Tap Tuan 10\QLBH_LOG.ldf',
+	size = 10mb,
+	maxsize = 100mb,
+	filegrowth = 10mb
+)
+
+use QLBH
+
+create table MatHang(
+	MaHang nchar(10) primary key,
+	TenHang nvarchar(30),
+	SoLuong int
+)
+
+create table NhatKyBanHang(
+	STT int,
+	Ngay datetime,
+	NguoiMua nvarchar(30),
+	MaHang nchar(10) primary key,
+	SoLuong int,
+	GiaBan money
+
+	constraint FK_MaHang foreign key(MaHang) references MatHang(MaHang)
+)
+
+insert into MatHang values(N'1', N'Keo', 100)
+insert into MatHang values(N'2', N'Banh', 200)
+insert into MatHang values(N'3', N'Thuoc', 100)
+
+select * from MatHang
+select * from NhatKyBanHang
+
+--a. trg_nhatkybanhang_insert. Trigger này có chức năng tự động
+--giảm số lượng hàng hiện có (Trong bảng MATHANG) khi một
+--mặt hàng nào đó được bán (tức là khi câu lệnh INSERT được thực
+--thi trên bảng NHATKYBANHANG).
+
+create trigger trg_nhatkybanhang_insert
+on NhatKyBanHang
+for insert
+as
+	begin
+		update MatHang
+		set MatHang.SoLuong = MatHang.SoLuong - inserted.SoLuong
+		from MatHang inner join inserted
+		on MatHang.MaHang = inserted.MaHang
+	end
+
+--TH test
+select * from MatHang
+select * from NhatKyBanHang
+
+insert into NhatKyBanHang(ngay, nguoimua, mahang, soluong,
+giaban) values('1999-2-9','ab','2',30,50)
+
+select * from MatHang
+select * from NhatKyBanHang
+
+--b. trg_nhatkybanhang_update_soluong được kích hoạt khi ta tiến
+--hành cập nhật cột SOLUONG cho một bản ghi của bảng
+--NHATKYBANHANG (lưu ý là chỉ cập nhật đúng một bản ghi).
+
+create trigger trg_nhatkybanhang_update_soluong
+on NhatKyBanHang
+for update
+as
+	begin
+		update MatHang
+		set MatHang.SoLuong = MatHang.SoLuong - (inserted.SoLuong - deleted.Soluong)
+		from deleted inner join inserted
+		on deleted.MaHang = inserted.MaHang
+		inner join MatHang
+		on MatHang.MaHang = inserted.MaHang
+	end
+
+--test
+select * from MatHang
+select * from NhatKyBanHang
+
+update NhatKyBanHang 
+set SoLuong = SoLuong + 20
+where MaHang = '2'
+
+select * from MatHang
+select * from NhatKyBanHang
+
+--c. Trigger dưới đây được kích hoạt khi câu lệnh INSERT được sử dụng
+--để bổ sung một bản ghi mới cho bảng NHATKYBANHANG. Trong
+--trigger này kiểm tra điều kiện hợp lệ của dữ liệu là số lượng hàng bán
+--ra phải nhỏ hơn hoặc bằng số lượng hàng hiện có. Nếu điều kiện này
+--không thoả mãn thì huỷ bỏ thao tác bổ sung dữ liệu.
+
+create trigger CauC
+on NhatKyBanHang
+for insert
+as
+	begin
+		declare @SoLuongBan int
+		declare @SoLuongCo int
+
+		set @SoLuongBan = (select SoLuong from inserted)
+		set @SoLuongCo = (select MatHang.SoLuong from MatHang inner join inserted on MatHang.MaHang = inserted.MaHang)
+
+		if(@SoLuongBan > @SoLuongCo)
+			begin
+				raiserror(N'Không đủ số lượng để xuất', 16, 1)
+				rollback transaction
+			end
+		else
+			begin
+				update MatHang
+				set MatHang.SoLuong = MatHang.SoLuong - inserted.SoLuong
+				from MatHang inner join inserted
+				on MatHang.MaHang = inserted.MaHang
+			end
+	end
+
+--test cau C
+select * from MatHang
+select * from NhatKyBanHang
+
+insert into NhatKyBanHang values (2, '2021-4-12', N'Nguyễn Nhật Minh', N'3', 30, 50000)
+
+select * from MatHang
+select * from NhatKyBanHang
+
+--d. Trigger dưới đây nhằm để kiểm soát lỗi update bảng
+--nhatkybanhang, nếu update >1 bản ghi thì thông báo lỗi(Trigger
+--chỉ làm trên 1 bản ghi), quay trở về. Ngược lại thì update lại số
+--lượng cho bảng mathang.
+
+create trigger trg_update_onerecord
+on NhatKyBanHang
+for update
+as
+	begin
+		if((select COUNT(*) from inserted) > 1)
+			begin
+				raiserror(N'Không sửa quá 1 dòng lệnh', 16, 1)
+				rollback transaction
+			end
+		else
+			begin
+				update MatHang
+				set MatHang.MaHang = MatHang.MaHang - (inserted.SoLuong - deleted.SoLuong)
+				from deleted inner join inserted
+				on deleted.MaHang = inserted.MaHang
+				inner join MatHang
+				on MatHang.MaHang = inserted.MaHang
+			end
+	end
+	
+--test
+
+select * from MatHang
+select * from NhatKyBanHang
+
+update NhatKyBanHang
+set SoLuong = SoLuong + 20
+
+select * from MatHang
+select * from NhatKyBanHang
+
+--e. Hay tao Trigger xoa 1 ban ghi bang nhatkybanhang, neu xoa nhieu hon
+--1 record thi hay thong bao loi xoa ban ghi, nguoc lai hay update bang
+--mathang voi cot so luong tang len voi ma hang da xoa o bang
+--nhatkybanhang.
+
+create trigger XoaBanGhi
+on NhatKyBanHang
+for delete
+as
+	begin
+		if((select COUNT(*) from deleted) > 1)
+			begin
+				raiserror(N'Chỉ được phép xóa 1 bản ghi', 16, 1)
+				rollback transaction
+			end
+		else
+			begin
+				update MatHang
+				set MatHang.SoLuong = MatHang.SoLuong + deleted.SoLuong
+				from MatHang inner join deleted
+				on MatHang.MaHang = deleted.MaHang
+			end
+	end
+
+--test
+
+select * from MatHang
+select * from NhatKyBanHang
+
+delete from NhatKyBanHang 
+where MaHang = '2'
+
+select * from MatHang
+select * from NhatKyBanHang
+
+--f. Tạo Trigger cập nhật bảng nhật ký bán hàng, nếu cập nhật nhiều hơn
+--1 bản ghi thông báo lỗi và phục hồi phiên giao dịch, ngược lại kiểm
+--tra xem nếu giá trị số lượng cập nhật <giá trị số lượng có thì thông báo
+--lỗi sai cập nhật, ngược lại nếu nếu giá trị số lượng cập nhật =giá trị số
+--lượng có thì thông báo không cần cập nhật ngược lại thì hãy cập nhật
+--giá trị.
+
+alter trigger update_nhatkybanhang
+on NhatKyBanHang
+for update
+as
+	begin
+		if((select COUNT(*) from inserted) > 1)
+			begin
+				raiserror(N'Chỉ được phép cập nhật 1 bản ghi', 16, 1)
+				rollback transaction
+			end
+		else
+			begin
+				declare @MaHang nchar(10)
+				declare @SoLuongTruoc int
+				declare @SoLuongSau int
+				declare @SoLuongCo int
+
+				set @MaHang = (select MaHang from inserted)
+				set @SoLuongTruoc = (select SoLuong from deleted where MaHang = @MaHang)
+				set @SoLuongSau = (select SoLuong from inserted where MaHang = @MaHang)
+				set @SoLuongCo = (select SoLuong from MatHang where MaHang = @MaHang)
+
+				if(@SoLuongCo >= (@SoLuongSau - @SoLuongTruoc))
+					begin
+						update MatHang
+						set SoLuong = SoLuong - (@SoLuongSau - @SoLuongTruoc)
+						where MaHang = @MaHang
+					end
+				else
+					begin
+						raiserror(N'Không đủ số lượng để xuất', 16, 1)
+						rollback transaction
+					end
+			end
+	end
+select * from MatHang
+select * from NhatKyBanHang
+
+update NhatKyBanHang
+set SoLuong = SoLuong + 30
+where MaHang = '3'
+select * from MatHang
+select * from NhatKyBanHang
