5988d0b94604ef41e68ece7ef9db33e5f17733dc
= <=>
2021-04-23 09:43:56 +0700

    ds

---
+++ On Tap De 2/Store Procedure.sql
@@ -0,0 +1,170 @@
+﻿--Bài nào liên quan đến Thủ Tục thì sài Procedure
+--Bài nào liên quan đến Hàm thì function
+--Bài nào liên quan đến khung nhìn thì view
+
+--Kiểu 1 Procedure Không đầu ra
+--create proc TenThuTuc(@tb1 kieudl1, @tb2 kieudl2, ...)
+--as
+	--begin
+		
+	--end
+
+--exec TenThuTuc N'asdasd', N'xzysad'
+--Procedure giống function nhưng cái xử lý thì không thiên về output(Tí nữa có thủ tục output)
+
+--Kiểu 2: Procedure Có đầu ra
+--create proc TenThuTuc(@tb1 kieudl1, @tb2 kieudl2, ..., @kq int output)
+--as
+	--begin
+		
+		--return @kq
+	--end
+
+--declare @Ketqua int
+--exec TenThuTuc N'asdasd', N'asdasdas', ..., @Ketqua output
+--select @Ketqua as N'Kết quả'
+
+use master
+go
+
+create database QLTruongHoc
+
+use QLTruongHoc
+go
+create table Khoa(
+	Makhoa nvarchar(10) primary key not null,
+	Tenkhoa nvarchar(50) not null,
+	Dienthoai nvarchar(20)
+)
+go
+
+create table Lop(
+	Malop nvarchar(10) primary key not null,
+	Tenlop nvarchar(50) not null,
+	Khoa nvarchar(20),
+	Hedt nvarchar(20),
+	Namnhaphoc int,
+	Makhoa nvarchar(10),
+
+	constraint FK_Makhoa foreign key(Makhoa) references Khoa(Makhoa)
+)
+go
+
+--Bài tập 1, Viết thủ tục nhập dữ liệu vào bảng KHOA với các tham biến:
+--makhoa,tenkhoa, dienthoai, hãy kiểm tra xem tên khoa đã tồn tại trước đó hay chưa,
+--nếu đã tồn tại đưa ra thông báo, nếu chưa tồn tại thì nhập vào bảng khoa, test với 2
+--trường hợp
+
+create procedure Baitap1(@Makhoa nvarchar(10), 
+						 @Tenkhoa nvarchar(50), 
+						 @Dienthoai nvarchar(20))
+as
+	begin
+		if(exists(select * from Khoa where Makhoa = @Makhoa))
+			begin
+				print 'Khoa đã tồn tại trong cơ sở dữ liệu'
+			end
+		else
+			begin
+				insert into Khoa values(@Makhoa, @Tenkhoa, @Dienthoai)
+			end
+	end
+
+select * from Khoa
+execute Baitap1 N'CNTT', N'Công Nghệ Thông Tin', N'12345678'
+
+--Bài tập 2. Hãy viết thủ tục nhập dữ liệu cho bảng Lop với các tham biến Malop,
+--Tenlop, Khoa,Hedt,Namnhaphoc,Makhoa nhập từ bàn phím.
+-- Kiểm tra xem tên lớp đã có trước đó chưa nếu có thì thông báo
+-- Kiểm tra xem makhoa này có trong bảng khoa hay không nếu không có thì thông báo
+-- Nếu đầy đủ thông tin thì cho nhập
+
+create procedure Baitap2(@Malop nvarchar(10), 
+						 @Tenlop nvarchar(50), 
+						 @Khoa nvarchar(20), 
+						 @Hedt nvarchar(20), 
+						 @Namnhaphoc int, 
+						 @Makhoa nvarchar(10))
+as
+	begin
+		if(exists(select * from Lop where Tenlop = @Tenlop))
+			begin
+				print 'Lớp ' + @Tenlop + ' đã tồn tại trong bảng Lớp'
+			end
+		else
+			if(not exists(select * from Khoa where Makhoa = @Makhoa))
+				begin
+					print 'Mã khoa ' + @Makhoa + ' chưa tồn tại trong bảng Khoa'
+				end
+			else
+				begin
+					insert into Lop values(@Malop, @Tenlop, @Khoa, @Hedt, @Namnhaphoc, @Makhoa)
+					print 'Thêm mới thành công'
+				end
+	end
+
+select * from Lop
+select * from Khoa
+execute Baitap2 N'KTPM1', N'Kĩ thuật phần mềm 1', N'K14', N'Đại Học', 2019, N'CNTT'
+
+--Bài tập 3, Viết thủ tục nhập dữ liệu vào bảng KHOA với các tham biến:
+--makhoa,tenkhoa, dienthoai, hãy kiểm tra xem tên khoa đã tồn tại trước đó hay chưa,
+--nếu đã tồn tại trả về giá trị 0, nếu chưa tồn tại thì nhập vào bảng khoa, test với 2
+--trường hợp.
+
+create procedure Baitap3(@Makhoa nvarchar(10), 
+                         @Tenkhoa nvarchar(50), 
+						 @Dienthoai nvarchar(20),
+						 @Ketqua int output)
+as
+	begin
+		if(exists(select * from Khoa where Tenkhoa = @Tenkhoa))
+			begin
+				set @Ketqua = 0
+			end
+		else
+			begin
+				insert into Khoa values(@Makhoa, @Tenkhoa, @Dienthoai)
+			end
+		return @Ketqua
+	end
+
+declare @KQ int
+execute Baitap3 N'CNTT1', N'Công Nghệ Thông Tin 2', N'123456789', @KQ output
+select @KQ as N'Kết quả'
+
+--Bài tập 4. Hãy viết thủ tục nhập dữ liệu cho bảng lop với các tham biến
+--malop,tenlop,khoa,hedt,namnhaphoc,makhoa.
+-- Kiểm tra xem tên lớp đã có trước đó chưa nếu có thì trả về 0.
+-- Kiểm tra xem makhoa này có trong bảng khoa hay không nếu không có thì tra ve 1.
+-- Nếu đầy đủ thông tin thì cho nhập và trả về 2
+
+create procedure Baitap4(@Malop nvarchar(10), 
+						 @Tenlop nvarchar(50), 
+						 @Khoa nvarchar(20), 
+						 @Hedt nvarchar(20), 
+						 @Namnhaphoc int, 
+						 @Makhoa nvarchar(10),
+						 @KQ int output)
+as
+	begin	
+		if(exists(select * from Lop where Tenlop = @Tenlop))
+			begin
+				set @KQ = 0
+			end
+		else
+			if(not exists(select * from Khoa where Makhoa = @Makhoa))
+				begin
+					set @KQ = 1
+				end
+			else
+				begin
+					insert into Lop values(@Malop, @Tenlop, @Khoa, @Hedt, @Namnhaphoc, @Makhoa)
+					set @KQ = 2
+				end
+		return @KQ
+	end
+
+declare @Ketqua int
+execute Baitap4 N'KTPM2', N'Kĩ thuật phần mềm 2', N'K14', N'Đại Học', 2019, N'CNTT', @Ketqua output
+select @Ketqua as N'Kết quả'
---
+++ On Tap De 2/Trigger.sql
@@ -0,0 +1,140 @@
+﻿use master
+
+create database QLBanHang
+
+use QLBanHang
+
+create table Hang(
+	MaHang nchar(10) primary key,
+	TenHang nvarchar(30),
+	SoLuong int,
+	GiaBan money
+)
+
+create table HoaDon(
+	MaHD nchar(10) primary key,
+	MaHang nchar(10),
+	SoLuongBan int,
+	NgayBan date
+
+	constraint FK_MaHang foreign key(MaHang) references Hang(MaHang)
+)
+
+insert into Hang values(N'H01', N'Ti Vi', 200, 5000000)
+insert into Hang values(N'H02', N'Tủ Lạnh', 200, 5000000)
+insert into Hang values(N'H03', N'Điều Hòa', 200, 5000000)
+insert into Hang values(N'H04', N'Bình Nóng Lạnh', 200, 5000000)
+insert into Hang values(N'H05', N'Máy Giặt', 200, 5000000)
+
+
+--delete from HoaDon
+--delete from Hang
+
+--Bài 1. Hãy tạo 1 trigger insert HoaDon, hãy kiểm tra xem mahang cần mua có tồn
+--tại trong bảng HANG không, nếu không hãy đưa ra thông báo.
+-- Nếu thỏa mãn hãy kiểm tra xem: soluongban <= soluong? Nếu không hãy
+--đưa ra thông báo.
+-- Ngược lại cập nhật lại bảng HANG với: soluong = soluong - soluongban
+
+create trigger insert_Hoadon
+on HoaDon
+for insert
+as
+	begin
+		if(not exists(select * from Hang inner join inserted on Hang.MaHang = inserted.MaHang))
+			begin
+				raiserror(N'Không tồn tại trong bảng Hàng',16,1)
+				rollback transaction--quay về trạng thái ban đầu
+			end
+		else
+			begin
+				declare @SoLuong int = (select SoLuong 
+										from Hang inner join inserted 
+										on Hang.MaHang = inserted.MaHang)
+				declare @SoLuongBan int = (select SoLuongBan from inserted)
+
+				if(@SoLuongBan <= @SoLuong)
+					begin
+						update Hang
+						set Hang.SoLuong = Hang.SoLuong - inserted.SoLuongBan
+						from Hang inner join inserted
+						on Hang.MaHang = inserted.MaHang
+					end
+				else
+					begin
+						raiserror(N'Không đủ Hàng',16,1)
+						rollback transaction--quay về trạng thái ban đầu
+					end
+			end
+		end
+
+--test bài 1
+select * from Hang
+select * from HoaDon
+
+--TH không tồn tại mặt hàng
+insert into HoaDon values(N'HD06', N'H06', 300, '2021-4-5')
+
+--TH không đủ số lượng
+insert into HoaDon values(N'HD06', N'H05', 300, '2021-4-5')
+
+--TH thông thường
+insert into HoaDon values(N'HD06', N'H05', 10, '2021-4-5')
+
+select * from Hang
+select * from HoaDon
+
+--Bài 2. Viết trigger kiểm soát việc Delete bảng HOADON, Hãy cập nhật lại soluong
+--trong bảng HANG với: SOLUONG =SOLUONG + DELETED.SOLUONGBAN
+
+create trigger Bai2 --nên làm lại bằng cách inner join để với trường hợp nhiều hóa đơn
+on HoaDon
+for delete
+as
+	begin
+		update Hang
+		set SoLuong = SoLuong + deleted.SoLuongBan 
+		from Hang inner join deleted
+		on Hang.MaHang = deleted.MaHang
+	end
+
+--TH test bài 2
+select * from Hang
+select * from HoaDon
+
+delete from HoaDon where MaHD = N'HD06'
+
+select * from HoaDon
+select * from Hang
+
+--Bài 3. Hãy viết trigger kiểm soát việc Update bảng HOADON. Khi đó hãy update
+--lại soluong trong bảng HANG.
+
+--note:
+--+) deleted: data trước update
+--+) inserted: data sau update
+
+create trigger Bai3
+on HoaDon
+for update
+as
+	begin
+		declare @SoLuongTruoc int
+		declare @SoLuongSau int
+
+		set @SoLuongTruoc = (select SoLuongBan from deleted)
+		set @SoLuongSau = (select SoLuongBan from inserted)
+
+		update Hang
+		set SoLuong = SoLuong - (@SoLuongSau - @SoLuongTruoc)
+		from Hang inner join inserted
+		on Hang.MaHang = inserted.MaHang
+	end
+
+--test bai 3
+select * from Hang
+select * from HoaDon
+
+update HoaDon set SoLuongBan = SoLuongBan + 15 where MaHD = 'HD06'
+
+select * from HoaDon
